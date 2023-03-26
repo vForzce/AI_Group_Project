adba6c651c7ee8c2570591f25b79eb1d2dbba025
Dylan Dong <61720063+ddong1230@users.noreply.github.com>
2023-03-26 17:09:22 -0400

    added pathfinding.py

--- generate_maze.py
+++ generate_maze.py
@@ -1,7 +1,7 @@
 import pygame as pg
+import pathfinding
 from random import random
 from collections import deque
-
 
 def get_rect(x, y):
     return x * TILE + 1, y * TILE + 1, TILE - 2, TILE - 2
@@ -20,24 +20,6 @@ def get_click_mouse_pos():
     click = pg.mouse.get_pressed()
     return (grid_x, grid_y) if click[0] else False
 
-
-def bfs(start, goal, graph):
-    queue = deque([start])
-    visited = {start: None}
-
-    while queue:
-        cur_node = queue.popleft()
-        if cur_node == goal:
-            break
-
-        next_nodes = graph[cur_node]
-        for next_node in next_nodes:
-            if next_node not in visited:
-                queue.append(next_node)
-                visited[next_node] = cur_node
-    return queue, visited
-
-
 RES = WIDTH, HEIGHT = 1202, 902
 TILE = 30
 cols, rows = WIDTH // TILE, HEIGHT // TILE
@@ -54,7 +36,7 @@ for y, row in enumerate(grid):
         if not col:
             graph[(x, y)] = graph.get((x, y), []) + get_next_nodes(x, y)
 
-# BFS settings
+# pathfinding settings
 start = (0, 0)
 goal = start
 queue = deque([start])
@@ -66,14 +48,15 @@ while True:
     # draw grid
     [[pg.draw.rect(sc, pg.Color('darkorange'), get_rect(x, y), border_radius=TILE // 5)
       for x, col in enumerate(row) if col] for y, row in enumerate(grid)]
-    # draw BFS work
+    
+    # draw visited nodes
     [pg.draw.rect(sc, pg.Color('forestgreen'), get_rect(x, y)) for x, y in visited]
     [pg.draw.rect(sc, pg.Color('darkslategray'), get_rect(x, y)) for x, y in queue]
 
-    # bfs, get path to mouse click
+    # get path to mouse click
     mouse_pos = get_click_mouse_pos()
     if mouse_pos and not grid[mouse_pos[1]][mouse_pos[0]]:
-        queue, visited = bfs(start, mouse_pos, graph)
+        queue, visited = pathfinding.dfs(start, mouse_pos, graph)
         goal = mouse_pos
 
     # draw path
@@ -83,6 +66,7 @@ while True:
         path_segment = visited[path_segment]
     pg.draw.rect(sc, pg.Color('blue'), get_rect(*start), border_radius=TILE // 3)
     pg.draw.rect(sc, pg.Color('magenta'), get_rect(*path_head), border_radius=TILE // 3)
+    
     # pygame necessary lines
     [exit() for event in pg.event.get() if event.type == pg.QUIT]
     pg.display.flip()
---
+++ pathfinding.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def bfs(start, goal, graph):
+    queue = deque([start])
+    visited = {start: None}
+
+    while queue:
+        cur_node = queue.popleft()
+        if cur_node == goal:
+            break
+
+        next_nodes = graph[cur_node]
+        for next_node in next_nodes:
+            if next_node not in visited:
+                queue.append(next_node)
+                visited[next_node] = cur_node
+    return queue, visited
+
+def dfs(start, goal, graph):
+    queue = deque([start])
+    visited = {start: None}
+
+    while queue:
+        cur_node = queue.pop()
+        if cur_node == goal:
+            break
+
+        next_nodes = graph[cur_node]
+        for next_node in next_nodes:
+            if next_node not in visited:
+                queue.append(next_node)
+                visited[next_node] = cur_node
+    return queue, visited
