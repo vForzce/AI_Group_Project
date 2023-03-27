cb51a068401aeed44961febfdc195053ac82286f
Dylan Dong <61720063+ddong1230@users.noreply.github.com>
2023-03-27 10:49:10 -0400

    press r to reset

--- generate_maze.py
+++ generate_maze.py
@@ -6,19 +6,18 @@ from collections import deque
 def get_rect(x, y):
     return x * TILE + 1, y * TILE + 1, TILE - 2, TILE - 2
 
-
-def get_next_nodes(x, y):
+def get_next_nodes(grid, x, y):
     check_next_node = lambda x, y: True if 0 <= x < cols and 0 <= y < rows and not grid[y][x] else False
     ways = [1, 0], [-1, 0], [0, 1], [0, -1]
     return [(x + dx, y + dy) for dx, dy in ways if check_next_node(x + dx, y + dy)]
 
-def new_grid():
+def new_grid(rows, cols):
     grid = [[1 if random() < 0.2 else 0 for _ in range(cols)] for _ in range(rows)]
     graph = {}
     for y, row in enumerate(grid):
         for x, col in enumerate(row):
             if not col:
-                graph[(x, y)] = graph.get((x, y), []) + get_next_nodes(x, y)
+                graph[(x, y)] = graph.get((x, y), []) + get_next_nodes(grid, x, y)
     return grid, graph
 
 def draw_mouse_cursor():
@@ -33,7 +32,7 @@ def draw_grid(grid):
     [[pg.draw.rect(sc, pg.Color('darkorange'), get_rect(x, y), border_radius=TILE // 5)
       for x, col in enumerate(row) if col] for y, row in enumerate(grid)]
     
-RES = WIDTH, HEIGHT = 1202, 902
+RES = WIDTH, HEIGHT = 902, 602
 TILE = 30
 cols, rows = WIDTH // TILE, HEIGHT // TILE
 
@@ -48,7 +47,7 @@ graph = {}
 for y, row in enumerate(grid):
     for x, col in enumerate(row):
         if not col:
-            graph[(x, y)] = graph.get((x, y), []) + get_next_nodes(x, y)
+            graph[(x, y)] = graph.get((x, y), []) + get_next_nodes(grid, x, y)
 
 # pathfinding settings
 start = (0, 0)
@@ -59,13 +58,13 @@ visited = {start: None}
 
 while True:
     draw_grid(grid)
+    draw_mouse_cursor()
     
     # draw visited nodes
     [pg.draw.rect(sc, pg.Color('forestgreen'), get_rect(x, y)) for x, y in visited]
     [pg.draw.rect(sc, pg.Color('darkslategray'), get_rect(x, y)) for x, y in queue]
 
-    # get path to mouse click
-    draw_mouse_cursor()
+    # draw click
     pg.draw.rect(sc, pg.Color('magenta'), get_rect(mouse_pos[0], mouse_pos[1]), border_radius=TILE // 3)
     
     # draw path
@@ -73,10 +72,11 @@ while True:
     while path_segment and path_segment in visited:
         pg.draw.rect(sc, pg.Color('white'), get_rect(*path_segment), TILE, border_radius=TILE // 3)
         path_segment = visited[path_segment]
+
     pg.draw.rect(sc, pg.Color('blue'), get_rect(*start), border_radius=TILE // 3)
     pg.draw.rect(sc, pg.Color('magenta'), get_rect(*path_head), border_radius=TILE // 3)
     
-    # pygame necessary lines
+    # interaction
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
@@ -88,14 +88,10 @@ while True:
             
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_r:
-                grid, graph = new_grid()
-                start = (0, 0)
-                mouse_pos = (0, 0)
-                goal = start
+                grid, graph = new_grid(rows, cols)
                 queue = deque([start])
                 visited = {start: None}
                 draw_grid(grid)
-                path_head, path_segment = (0, 0), (0, 0)
                 
             if event.key == pg.K_d:
                 if mouse_pos and not grid[mouse_pos[1]][mouse_pos[0]]:
