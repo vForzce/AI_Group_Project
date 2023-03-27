07feb065db0e1d7a4beadfac7376274fd0d4f451
Dylan Dong <61720063+ddong1230@users.noreply.github.com>
2023-03-26 22:08:01 -0400

    update maze algo
    current issue: unable to regenerate maze

--- generate_maze.py
+++ generate_maze.py
@@ -9,17 +9,30 @@ def get_rect(x, y):
 
 def get_next_nodes(x, y):
     check_next_node = lambda x, y: True if 0 <= x < cols and 0 <= y < rows and not grid[y][x] else False
-    ways = [-1, 0], [0, -1], [1, 0], [0, 1], [-1, -1], [1, -1], [1, 1], [-1, 1]
+    ways = [1, 0], [-1, 0], [0, 1], [0, -1]
     return [(x + dx, y + dy) for dx, dy in ways if check_next_node(x + dx, y + dy)]
 
+def new_grid():
+    grid = [[1 if random() < 0.2 else 0 for _ in range(cols)] for _ in range(rows)]
+    graph = {}
+    for y, row in enumerate(grid):
+        for x, col in enumerate(row):
+            if not col:
+                graph[(x, y)] = graph.get((x, y), []) + get_next_nodes(x, y)
+    return grid, graph
 
-def get_click_mouse_pos():
+def draw_mouse_cursor():
     x, y = pg.mouse.get_pos()
     grid_x, grid_y = x // TILE, y // TILE
     pg.draw.rect(sc, pg.Color('red'), get_rect(grid_x, grid_y))
-    click = pg.mouse.get_pressed()
-    return (grid_x, grid_y) if click[0] else False
 
+def draw_grid(grid):
+    # fill screen
+    sc.fill(pg.Color('black'))
+    # draw grid
+    [[pg.draw.rect(sc, pg.Color('darkorange'), get_rect(x, y), border_radius=TILE // 5)
+      for x, col in enumerate(row) if col] for y, row in enumerate(grid)]
+    
 RES = WIDTH, HEIGHT = 1202, 902
 TILE = 30
 cols, rows = WIDTH // TILE, HEIGHT // TILE
@@ -27,8 +40,9 @@ cols, rows = WIDTH // TILE, HEIGHT // TILE
 pg.init()
 sc = pg.display.set_mode(RES)
 clock = pg.time.Clock()
+
 # grid
-grid = [[1 if random() < 0.2 else 0 for col in range(cols)] for row in range(rows)]
+grid = [[1 if random() < 0.2 else 0 for _ in range(cols)] for _ in range(rows)]
 # dict of adjacency lists
 graph = {}
 for y, row in enumerate(grid):
@@ -38,27 +52,22 @@ for y, row in enumerate(grid):
 
 # pathfinding settings
 start = (0, 0)
+mouse_pos = (0, 0)
 goal = start
 queue = deque([start])
 visited = {start: None}
 
 while True:
-    # fill screen
-    sc.fill(pg.Color('black'))
-    # draw grid
-    [[pg.draw.rect(sc, pg.Color('darkorange'), get_rect(x, y), border_radius=TILE // 5)
-      for x, col in enumerate(row) if col] for y, row in enumerate(grid)]
+    draw_grid(grid)
     
     # draw visited nodes
     [pg.draw.rect(sc, pg.Color('forestgreen'), get_rect(x, y)) for x, y in visited]
     [pg.draw.rect(sc, pg.Color('darkslategray'), get_rect(x, y)) for x, y in queue]
 
     # get path to mouse click
-    mouse_pos = get_click_mouse_pos()
-    if mouse_pos and not grid[mouse_pos[1]][mouse_pos[0]]:
-        queue, visited = pathfinding.dfs(start, mouse_pos, graph)
-        goal = mouse_pos
-
+    draw_mouse_cursor()
+    pg.draw.rect(sc, pg.Color('magenta'), get_rect(mouse_pos[0], mouse_pos[1]), border_radius=TILE // 3)
+    
     # draw path
     path_head, path_segment = goal, goal
     while path_segment and path_segment in visited:
@@ -68,6 +77,34 @@ while True:
     pg.draw.rect(sc, pg.Color('magenta'), get_rect(*path_head), border_radius=TILE // 3)
     
     # pygame necessary lines
-    [exit() for event in pg.event.get() if event.type == pg.QUIT]
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+            
+        if pg.mouse.get_pressed()[0]: # LEFT
+            x, y = pg.mouse.get_pos()
+            grid_x, grid_y = x // TILE, y // TILE
+            mouse_pos = (grid_x, grid_y)
+            
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_r:
+                grid, graph = new_grid()
+                start = (0, 0)
+                mouse_pos = (0, 0)
+                goal = start
+                queue = deque([start])
+                visited = {start: None}
+                draw_grid(grid)
+                path_head, path_segment = (0, 0), (0, 0)
+                
+            if event.key == pg.K_d:
+                if mouse_pos and not grid[mouse_pos[1]][mouse_pos[0]]:
+                    queue, visited = pathfinding.dfs(start, mouse_pos, graph)
+                    goal = mouse_pos
+            if event.key == pg.K_b:
+                if mouse_pos and not grid[mouse_pos[1]][mouse_pos[0]]:
+                    queue, visited = pathfinding.bfs(start, mouse_pos, graph)
+                    goal = mouse_pos    
+
     pg.display.flip()
     clock.tick(30)
